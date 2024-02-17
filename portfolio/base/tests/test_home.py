8a7bc75ebd644f1d75b5d2d233af25d93fbbc1c1
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2024-02-17 17:17:16 +0100

    Adds tests to home page

--- portfolio/base/tests/test_home.py
+++ portfolio/base/tests/test_home.py
@@ -1,6 +1,7 @@
 import pytest
 from django.urls import reverse
 
+from portfolio.django_assertions import assert_contains
 
 @pytest.fixture
 def response(client):
@@ -10,3 +11,19 @@ def response(client):
 
 def test_status_code(response):
     assert response.status_code == 200
+
+
+def test_home_link(response):
+    assert_contains(response, f'href="{reverse('base:home')}">Home</a>')
+
+
+def test_knowledge_link(response):
+    assert_contains(response, f'href="{reverse('knowledge:knowledge')}">Knowledge</a>')
+
+
+def test_projects_link(response):
+    assert_contains(response, f'href="{reverse('projects:projects')}">Projects</a>')
+
+
+def test_contains_image(response):
+    assert_contains(response, f'<img src="img_profile.jpg" alt="Photo of me">')
