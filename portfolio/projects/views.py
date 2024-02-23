f7e4e3b850cfd9caa069c662d29386e1554a35fb
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2024-02-23 17:45:33 +0100

    Adjust to better practices, Adds projects home view

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from django.shortcuts import render
 
 
 @require_http_methods(["GET"])
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'projects/home.html')
