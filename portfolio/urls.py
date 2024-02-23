15c7c6c9f245249908650feb1442aabcfef2da52
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2024-02-23 17:30:35 +0100

    Include projects urls in project

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('portfolio.base.urls')),
     path('knowledge/', include('portfolio.knowledge.urls')),
+    path('projects/', include('portfolio.projects.urls')),
 ]
