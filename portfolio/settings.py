4586641eab2a290280fa2b1f6c3c6ce43e2ae221
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2024-02-27 22:20:46 +0100

    Try fix javascript file by an easier way

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import os.path
 
 from decouple import config, Csv
-import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,7 +104,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "portfolio/base/static")]
 
@@ -119,5 +118,3 @@ STORAGES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-mimetypes.add_type("text/javascript", ".js", True)
