2aa43f78c9b9d8841ddf43733df46c1075b98c79
Rafael Abreu Fonseca <rafael.abreu.fonseca@gmail.com>
2024-02-27 21:51:49 +0100

    Adds Mime to JS files in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os.path
 
 from decouple import config, Csv
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,3 +119,5 @@ STORAGES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+mimetypes.add_type("text/javascript", ".js", True)
